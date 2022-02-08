b08966f05c9c55a6f62bf8336edb59df57bfd796
Evan Schoening <evan.schoening@gmail.com>
2022-02-07 16:04:56 -0800

    error handling on run function

--- Documents/Rust/minigrep/src/main.rs
+++ Documents/Rust/minigrep/src/main.rs
@@ -1,19 +1,44 @@
 use std::env;
 use std::fs;
+use std::error::Error;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap();
 
-    println!("query = {}", query);
-    println!("filename = {}", filename);
+    println!("query = {}", config.query);
+    println!("filename = {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("unable to read file");
+    run(config);
 
-    println!("Text of tile:\n{}", contents);
+  
 
-    // create a parse command function for query and filename s
+    
+    // create a parse command function for query and filename
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 3 {
+            return Err("Not enough args".to_string())
+        } 
+        let query = &args[1].clone();
+        let filename = &args[2].clone();
+
+        Ok(Config { query: query.to_string() , filename: filename.to_string() })
+    }    
+}
+
+fn run(config: Config) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    
+    println!("Text of tile:\n{}", contents);
+    Ok(contents)
+}
+
